ea67c150f328a06939d03778bc29d7e994feb8fb
Jim Lin <jimlintw922@gmail.com>
2018-02-22 11:31:16 +0800

    add build-helper.sh

---
+++ build-helper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+function show_env() {
+    echo tty
+    grep tty usr/lib/python2.7/site-packages/diskimage_builder/elements/bootloader/finalise.d/50-bootloader
+    echo
+}
+
+if [ $# -eq 1 ]; then
+  fn=$(echo $1 | sed 's/-/_/g')
+  $fn
+fi 
