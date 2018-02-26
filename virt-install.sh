5347032695172c29837783e9b8b79552ecf75c6e
Jim Lin <jimlintw922@gmail.com>
2018-02-26 16:52:24 +0800

    update virt-install.sh

--- virt-install.sh
+++ virt-install.sh
@@ -10,3 +10,6 @@ if [ -z $RAW ]; then
 fi
 
 virt-install --connect=qemu:///system --name=centos7 --ram=512 --vcpus=1 --disk path=$RAW,format=raw --import --network network:default --vnc &
+
+sleep 3
+virsh console centos7
